cdde1f97c47d0f7205c1b84828c61223f6dfbd43
rdeese <rdeese@hmc.edu>
2017-04-19 03:36:05 -0500

    First commit!

---
+++ count_tokens.py
@@ -0,0 +1,59 @@
+import os
+import csv
+import json
+from collections import defaultdict
+import regex
+
+lol_regex = regex.compile(r"(^|[^A-z]+)l+o+l+(l*o*l)*($|[^A-z]+)", regex.IGNORECASE)
+repeated_char_regex = regex.compile(r"[^.\s]*(?P<char>[^.\s])\g<char>{2,}[^.\s]*", regex.IGNORECASE)
+
+def lol_matcher(comment):
+    return [match[0] for match in lol_regex.finditer(comment)]
+
+def repeated_character_matcher(comment):
+    return [match[0] for match in repeated_char_regex.finditer(comment)]
+
+
+def count_all_in_csv(reader):
+    """ count the things """
+    token_counts = {
+        'lol': { 'matcher': lol_matcher, 'count': 0 }
+    }
+
+    repeats_dict = defaultdict(lambda: 0)
+    
+    reader.__next__() # pop off the header
+
+    for entry in reader:
+        comment = entry[3]
+        repeats = repeated_character_matcher(comment)
+        for repeat in repeats:
+            repeats_dict[repeat] += 1
+
+        for token in token_counts.keys():
+            token_counts[token]['count'] += len(token_counts[token]['matcher'](comment))
+
+    return ([[token, value['count']] for token, value in token_counts.items()],
+            sorted(repeats_dict.items(), key=lambda x: x[1]))
+
+def main():
+    csv_dir = "csv"
+    count_dir = "output"
+    for filename in os.listdir(csv_dir):
+        name, ext = os.path.splitext(filename)
+        if ext != ".csv":
+            continue;
+        with open(os.path.join(csv_dir, filename), 'r') as csv_file:
+            token_counts, repeat_counts = count_all_in_csv(csv.reader(csv_file))
+        with open(os.path.join(count_dir, name + "-tokens.csv"), 'w') as outfile:
+            writer = csv.writer(outfile)
+            writer.writerow(['token', 'count'])
+            writer.writerows(token_counts)
+        with open(os.path.join(count_dir, name + "-repeats.csv"), 'w') as outfile:
+            writer = csv.writer(outfile)
+            writer.writerow(['word', 'count'])
+            writer.writerows(repeat_counts)
+
+
+if __name__ == '__main__':
+    main()
---
+++ test_count_tokens.py
@@ -0,0 +1,58 @@
+import unittest
+from count_tokens import *
+
+class TestMatchers(unittest.TestCase):
+    def test_lol_matcher(self):
+        self.assertEqual(lol_matcher("lol"), ["lol"])
+        self.assertEqual(lol_matcher("lolol"), ["lolol"])
+
+        self.assertEqual(len(lol_matcher("ol")), 0)
+        self.assertEqual(len(lol_matcher("lo")), 0)
+        self.assertEqual(len(lol_matcher("lollipop")), 0)
+        self.assertEqual(len(lol_matcher("olol")), 0)
+
+        self.assertEqual(lol_matcher("lolllll"), ["lolllll"])
+        self.assertEqual(lol_matcher("llllllol"), ["llllllol"])
+        self.assertEqual(lol_matcher("looooool"), ["looooool"])
+
+        self.assertEqual(len(lol_matcher("lolooolollollo")), 0)
+        self.assertEqual(lol_matcher("lolooolollollol"), ["lolooolollollol"])
+
+        self.assertEqual(lol_matcher("lol!"), ["lol!"])
+        self.assertEqual(lol_matcher("lol!i cant even"), ["lol!"])
+        self.assertEqual(lol_matcher("wow!lol."), ["!lol."])
+
+        # problematic?
+        self.assertEqual(len(lol_matcher("omglol")), 0)
+
+    def test_repeated_character_matcher(self):
+        self.assertEqual(len(repeated_character_matcher("haha")), 0)
+
+        self.assertEqual(len(repeated_character_matcher("haa")), 0)
+
+        matches = repeated_character_matcher("oook")
+        self.assertEqual(matches, ["oook"])
+
+        matches = repeated_character_matcher("haahahaaa ha")
+        self.assertEqual(matches, ["haahahaaa"])
+
+        matches = repeated_character_matcher("loooolllll")
+        self.assertEqual(matches, ["loooolllll"])
+
+        matches = repeated_character_matcher("!!!!!!!")
+        self.assertEqual(matches, ["!!!!!!!"])
+        
+        self.assertEqual(len(repeated_character_matcher("...")), 0)
+        self.assertEqual(len(repeated_character_matcher("a   b")), 0)
+
+        matches = repeated_character_matcher(":DDDD")
+        self.assertEqual(matches, [":DDDD"])
+
+        matches = repeated_character_matcher("wOoOoO")
+        self.assertEqual(matches, ["wOoOoO"])
+
+        matches = repeated_character_matcher("ohhh yyyeaaah")
+        self.assertEqual(matches, ["ohhh", "yyyeaaah"])
+
+if __name__ == '__main__':
+    unittest.main()
