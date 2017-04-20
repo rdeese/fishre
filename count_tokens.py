03c73c2763ab0b7da2a8bfaab3ebbcaad4fd8a4d
rdeese <rdeese@hmc.edu>
2017-04-20 01:08:33 -0500

    DRY out matcher & count object creation.

--- count_tokens.py
+++ count_tokens.py
@@ -17,33 +17,7 @@ def build_replacement_form_matcher(word, ignorecase=True,
         re = regex.compile(expression)
     return lambda comment: [match[0] for match in re.finditer(comment)]
 
-you_matcher = build_replacement_form_matcher("you")
-u_matcher = build_replacement_form_matcher("u")
-your_matcher = build_replacement_form_matcher("your")
-ur_matcher = build_replacement_form_matcher("ur")
-I_matcher = build_replacement_form_matcher("I", ignorecase=False)
-i_matcher = build_replacement_form_matcher("i", ignorecase=False)
-for_matcher = build_replacement_form_matcher("for")
-number_4_matcher = build_replacement_form_matcher("4", template=r"(^|\s){}($|\s)")
-before_matcher = build_replacement_form_matcher("before")
-be4_matcher = build_replacement_form_matcher("be4")
-b4_matcher = build_replacement_form_matcher("b4")
-though_matcher = build_replacement_form_matcher("though")
-tho_matcher = build_replacement_form_matcher("tho")
-because_matcher = build_replacement_form_matcher("because")
-becuz_matcher = build_replacement_form_matcher("becuz")
-bcuz_matcher = build_replacement_form_matcher("bcuz")
-cuz_matcher = build_replacement_form_matcher("cuz")
-bc_matcher = build_replacement_form_matcher("bc")
-bslashc_matcher = build_replacement_form_matcher("b/c")
-what_matcher = build_replacement_form_matcher("what")
-wat_matcher = build_replacement_form_matcher("wat")
-whut_matcher = build_replacement_form_matcher("whut")
-wut_matcher = build_replacement_form_matcher("wut")
-people_matcher = build_replacement_form_matcher("people")
-ppl_matcher = build_replacement_form_matcher("ppl")
-be_matcher = build_replacement_form_matcher("be")
-b_matcher = build_replacement_form_matcher("b")
+
 
 # WORDS WITH A REPEATED CHARATER
 repeated_char_regex = regex.compile(r"[^.\s]*(?P<char>[^.\s])\g<char>{2,}[^.\s]*", regex.IGNORECASE)
@@ -54,38 +28,30 @@ def lol_matcher(comment):
 def repeated_character_matcher(comment):
     return [match[0] for match in repeated_char_regex.finditer(comment)]
 
+def build_count_object(matcher):
+    return {
+        'count': 0,
+        'matches': [],
+        'matcher': matcher
+    }
 
 def count_all_in_csv(reader):
     """ count the things """
+    replacement_forms = [
+        "for", "you", "u", "your", "ur", "before", "be4", "b4",
+        "though", "tho", "because", "becuz", "bcuz", "cuz", "bc",
+        "b/c", "what", "wat", "whut", "wut", "people", "ppl", "be", "b"
+    ]
+
     token_counts = OrderedDict()
-    token_counts['lol'] = { 'matcher': lol_matcher, 'count': 0, 'matches': [] }
-    token_counts['you'] = { 'matcher': you_matcher, 'count': 0, 'matches': [] }
-    token_counts['u'] = { 'matcher': u_matcher, 'count': 0, 'matches': [] }
-    token_counts['your'] = { 'matcher': your_matcher , 'count': 0, 'matches': [] }
-    token_counts['ur'] = { 'matcher': ur_matcher , 'count': 0, 'matches': [] }
-    token_counts['I'] = { 'matcher': I_matcher , 'count': 0, 'matches': [] }
-    token_counts['i'] = { 'matcher': i_matcher , 'count': 0, 'matches': [] }
-    token_counts['for'] = { 'matcher': for_matcher , 'count': 0, 'matches': [] }
-    token_counts['4'] = { 'matcher': number_4_matcher , 'count': 0, 'matches': [] }
-    token_counts['before'] = { 'matcher': before_matcher , 'count': 0, 'matches': [] }
-    token_counts['be4'] = { 'matcher': be4_matcher , 'count': 0, 'matches': [] }
-    token_counts['b4'] = { 'matcher': b4_matcher , 'count': 0, 'matches': [] }
-    token_counts['though'] = { 'matcher': though_matcher , 'count': 0, 'matches': [] }
-    token_counts['tho'] = { 'matcher': tho_matcher , 'count': 0, 'matches': [] }
-    token_counts['because'] = { 'matcher': because_matcher , 'count': 0, 'matches': [] }
-    token_counts['becuz'] = { 'matcher': becuz_matcher , 'count': 0, 'matches': [] }
-    token_counts['bcuz'] = { 'matcher': bcuz_matcher , 'count': 0, 'matches': [] }
-    token_counts['cuz'] = { 'matcher': cuz_matcher , 'count': 0, 'matches': [] }
-    token_counts['bc'] = { 'matcher': bc_matcher , 'count': 0, 'matches': [] }
-    token_counts['b/c'] = { 'matcher': bslashc_matcher , 'count': 0, 'matches': [] }
-    token_counts['what'] = { 'matcher': what_matcher , 'count': 0, 'matches': [] }
-    token_counts['wat'] = { 'matcher': wat_matcher , 'count': 0, 'matches': [] }
-    token_counts['whut'] = { 'matcher': whut_matcher , 'count': 0, 'matches': [] }
-    token_counts['wut'] = { 'matcher': wut_matcher , 'count': 0, 'matches': [] }
-    token_counts['people'] = { 'matcher': people_matcher , 'count': 0, 'matches': [] }
-    token_counts['ppl'] = { 'matcher': ppl_matcher , 'count': 0, 'matches': [] }
-    token_counts['be'] = { 'matcher': be_matcher , 'count': 0, 'matches': [] }
-    token_counts['b'] = { 'matcher': b_matcher , 'count': 0, 'matches': [] }
+    token_counts['lol'] = build_count_object(lol_matcher)
+
+    token_counts["I"] = build_count_object(build_replacement_form_matcher("I", ignorecase=False))
+    token_counts["i"] = build_count_object(build_replacement_form_matcher("i", ignorecase=False))
+    token_counts["4"] = build_count_object(build_replacement_form_matcher("4", template=r"(^|\s){}($|\s)"))
+
+    for form in replacement_forms:
+        token_counts[form] = build_count_object(build_replacement_form_matcher(form))
 
     repeats_dict = defaultdict(lambda: 0)
     
