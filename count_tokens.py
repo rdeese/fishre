f1beb3f138c4743d9651fc8b350e8cdae3aeca9d
rdeese <rdeese@hmc.edu>
2017-04-20 21:19:56 -0500

    Condense results into single output table.

--- count_tokens.py
+++ count_tokens.py
@@ -2,6 +2,7 @@ import os
 import csv
 import json
 from collections import defaultdict, OrderedDict
+from functools import reduce
 import regex
 
 # ACRONYMS
@@ -48,57 +49,93 @@ def build_count_object(matcher):
         'matcher': matcher
     }
 
-def build_token_counts_object():
+def build_acronym_counts_object():
     acronyms = [
         "omg", "omfg", "lmao", "lmfao", "btw","wtf",
         "nvm", "np", "smh", "ftw"
     ]
+
+    acronym_counts = OrderedDict()
+    acronym_counts['lol'] = build_count_object(lol_matcher)
+    acronym_counts["k"] = build_count_object(build_acronym_matcher("k", template=r"(^|\s){}($|\s)"))
+    acronym_counts["kk"] = build_count_object(build_acronym_matcher("(kk|kkkk+)", template=r"(^|\s){}($|\s)"))
+    for acronym in acronyms:
+        acronym_counts[acronym] = build_count_object(build_acronym_matcher(acronym))
+    return acronym_counts
+
+def build_form_counts_object():
     replacement_forms = [
         "for", "you", "u", "your", "ur", "before", "be4", "b4",
         "though", "tho", "because", "becuz", "bcuz", "cuz", "bc",
         "b/c", "what", "wat", "whut", "wut", "people", "ppl", "be", "b"
     ]
 
-    token_counts = OrderedDict()
-
-    token_counts['lol'] = build_count_object(lol_matcher)
-    token_counts["k"] = build_count_object(build_acronym_matcher("k", template=r"(^|\s){}($|\s)"))
-    token_counts["kk"] = build_count_object(build_acronym_matcher("(kk|kkkk+)", template=r"(^|\s){}($|\s)"))
-    for acronym in acronyms:
-        token_counts[acronym] = build_count_object(build_acronym_matcher(acronym))
-
-    token_counts["I"] = build_count_object(build_replacement_form_matcher("I", ignorecase=False))
-    token_counts["i"] = build_count_object(build_replacement_form_matcher("i", ignorecase=False))
-    token_counts["4"] = build_count_object(build_replacement_form_matcher("4", template=r"(^|\s){}($|\s)"))
+    form_counts = OrderedDict()
+    form_counts["I"] = build_count_object(build_replacement_form_matcher("I", ignorecase=False))
+    form_counts["i"] = build_count_object(build_replacement_form_matcher("i", ignorecase=False))
+    form_counts["4"] = build_count_object(build_replacement_form_matcher("4", template=r"(^|\s){}($|\s)"))
     for form in replacement_forms:
-        token_counts[form] = build_count_object(build_replacement_form_matcher(form))
+        form_counts[form] = build_count_object(build_replacement_form_matcher(form))
+    return form_counts
 
-    token_counts["haha"] = build_count_object(haha_matcher)
-    token_counts["hehe"] = build_count_object(hehe_matcher)
+def build_laughter_counts_object():
+    laughter_counts = OrderedDict()
+    laughter_counts["haha"] = build_count_object(haha_matcher)
+    laughter_counts["hehe"] = build_count_object(hehe_matcher)
+    return laughter_counts
 
-    return token_counts
+def format_token_count_table(token_counts, table_name):
+    table_header = [[table_name, '', ''], ['token', 'count', 'matches']]
+    return table_header + [[token, value['count'], value['matches']]
+                           for token, value in token_counts.items()]
 
+def format_repeats_table(repeats_dict):
+    table_header = [["WORDS WITH REPEATED LETTERS", ""], ['word', 'count']]
+    return table_header + sorted([list(pair) for pair in repeats_dict.items()], reverse=True, key=lambda x: x[1])
+
+def format_output_table(tables):
+    max_rows = max([len(table) for table in tables])
+    for table in tables:
+        num_padding_rows = max_rows - len(table)
+        num_cols = len(table[0])
+        table.extend(num_padding_rows*[num_cols*['']])
+    combined_table = zip(*tables)
+    return [reduce(lambda a,b: a+b, row) for row in combined_table]
 
 def count_all_in_csv(reader):
-    token_counts = build_token_counts_object()
+    acronym_counts = build_acronym_counts_object()
+    form_counts = build_form_counts_object()
+    laughter_counts = build_laughter_counts_object()
     repeats_dict = defaultdict(lambda: 0)
+    total_length = 0
+    num_comments = 0
     
     reader.__next__() # pop off the header
 
     for entry in reader:
         comment = entry[3] or entry[10]
+
+        if len(comment) != 0:
+            num_comments += 1
+        total_length += len(comment)
+
         repeats = repeated_character_matcher(comment)
         for repeat in repeats:
             repeats_dict[repeat] += 1
 
-        for token in token_counts.keys():
-            matches = token_counts[token]['matcher'](comment)
-            token_counts[token]['count'] += len(matches)
-            token_counts[token]['matches'].extend(matches)
+        for token_counts in [acronym_counts, form_counts, laughter_counts]:
+            for token in token_counts.keys():
+                matches = token_counts[token]['matcher'](comment)
+                token_counts[token]['count'] += len(matches)
+                token_counts[token]['matches'].extend(matches)
 
-    return ([[token, value['count'], value['matches']]
-             for token, value in token_counts.items()],
-            sorted(repeats_dict.items(), reverse=True, key=lambda x: x[1]))
+    return (
+        format_token_count_table(acronym_counts, "ACRONYMS"),
+        format_token_count_table(form_counts, "REPLACEMENT FORMS"),
+        format_token_count_table(laughter_counts, "LAUGHTER"),
+        format_repeats_table(repeats_dict),
+        [["AVERAGE COMMENT LENGTH"], [total_length/num_comments]]
+    )
 
 def main():
     csv_dir = "csv"
@@ -108,15 +145,11 @@ def main():
         if ext != ".csv":
             continue;
         with open(os.path.join(csv_dir, filename), 'r') as csv_file:
-            token_counts, repeat_counts = count_all_in_csv(csv.reader(csv_file))
-        with open(os.path.join(count_dir, name + "-tokens.csv"), 'w') as outfile:
+            tables = count_all_in_csv(csv.reader(csv_file))
+            output_table = format_output_table(tables)
+        with open(os.path.join(count_dir, name + "-counts.csv"), 'w') as outfile:
             writer = csv.writer(outfile)
-            writer.writerow(['token', 'count', 'matches'])
-            writer.writerows(token_counts)
-        with open(os.path.join(count_dir, name + "-repeats.csv"), 'w') as outfile:
-            writer = csv.writer(outfile)
-            writer.writerow(['word', 'count'])
-            writer.writerows(repeat_counts)
+            writer.writerows(output_table)
 
 
 if __name__ == '__main__':
